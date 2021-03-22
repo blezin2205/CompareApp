63f961852dacb92057e8fbf2da13cd59d8dd285d
Blezin <blezin2205@gmail.com>
2021-03-22 19:28:28 +0200

    Initial Commit

---
+++ TestCompareApp/TestCompareAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCompareAppApp.swift
+//  TestCompareApp
+//
+//  Created by Blezin on 22.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct TestCompareAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
